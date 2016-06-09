e0d6b819c8a419c960e55bcf4453cc270316c928
openhardwarecoza <peter.plaaswerf@gmail.com>
2016-06-09 22:22:05 +0200

    Jog and DRO via Smoothie Ethernet works!

--- public/js/smoothie.js
+++ public/js/smoothie.js
@@ -1,5 +1,6 @@
 var queryLoop;
 var connectMode;
+var scanned, scanok, scanfail;
 
 function initSmoothie() {
   $('#connectVia').change(function() {
@@ -8,10 +9,12 @@ function initSmoothie() {
         connectMode = "USB";
         $('#usbConnect').show();
         $('#ethernetConnect').hide();
+        $('#playBtn').show();
     } else if (connectVia == "Ethernet") {
       connectMode = "ETH";
       $('#usbConnect').hide();
       $('#ethernetConnect').show();
+      $('#playBtn').hide();
     }
   });
 
@@ -19,9 +22,15 @@ function initSmoothie() {
     e.stopPropagation();
   });
 
+  $('#scansubnet').on('click', function(e) {
+      e.preventDefault();
+      e.stopPropagation(); // only neccessary if something above is listening to the (default-)event too
+      scanSubnet()
+  });
+
   $('#ethConnect').on('click', function() {
     var smoothieIp = $('#smoothieIp').val();
-
+    localStorage.setItem('smoothieIp', smoothieIp);
     $.ajax({
      type: 'GET',
         url: 'http://' +smoothieIp + '/',
@@ -33,7 +42,7 @@ function initSmoothie() {
             isConnected = true;
             $('#ethConnectStatus').html("Ethernet OK")
             $('#syncstatus').html('Eth Connected');
-            queryLoop = setInterval(function(){ runCommand('M114') }, 300);
+            queryLoop = setInterval(function(){ getStatus() }, 300);
           }
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
@@ -43,20 +52,12 @@ function initSmoothie() {
     });
 
   });
-
 };
 
-function checkIsIPV4(entry) {
-  var blocks = entry.split(".");
-  if(blocks.length === 4) {
-    return blocks.every(function(block) {
-      return parseInt(block,10) >=0 && parseInt(block,10) <= 255;
-    });
-  }
-  return false;
-}
-
 function scanSubnet() {
+  scanned = 254;
+  scanok = 0;
+  scanfail = 0;
   $("#foundIp").empty();
   var subnet1 = $("#subnet1").val();
   var subnet2 = $("#subnet2").val();
@@ -79,8 +80,6 @@ function scanSubnet() {
   localStorage.setItem("subnet1", subnet1);
   localStorage.setItem("subnet2", subnet2);
   localStorage.setItem("subnet3", subnet3);
-
-
 };
 
 function  scanIP(ip) {
@@ -91,102 +90,98 @@ function  scanIP(ip) {
   var posting = $.post(url, cmd);
   // Put the results in a div
   posting.done(function(data) {
-
+    scanned = scanned - 1;
+    scanok += 1
+    $("#scannumber").html('Scanning: <span style="color: #00cc00">'+scanok+ '</span>+<span style="color: #cc0000">'+scanfail+ '</span> done. '+scanned+' to go.' )
       $.each(data.split('\n'), function(index) {
-          printLog(this, msgcolor)
+        printLog(this, msgcolor)
+        var pattern = /Build version: (.*), Build date: (.*), MCU: (.*), System Clock: (.*)/;
+         // test the pattern
+         var matches = this.match(pattern);
+         if (matches) {
+             // split branch-hash on dash
+             var branchgit = matches[1].split('-');
 
-          var pattern = /Build version: (.*), Build date: (.*), MCU: (.*), System Clock: (.*)/;
-
-           // test the pattern
-           var matches = this.match(pattern);
-
-           if (matches) {
-               // split branch-hash on dash
-               var branchgit = matches[1].split('-');
-
-               var branch = branchgit[0];
-               var hash   = branchgit[1];
-               var date   = matches[2];
-               var mcu    = matches[3];
-               var clock  = matches[4];
-               $("#foundIp").append("<div class='panel panel-primary'><div class='panel-heading'><h3 class='panel-title'><a onclick='setIP(\""+ip+"\")' href='#'>"+ip+"</a></h3></div><div class='panel-body'>MCU: <kbd>"+mcu+ " @ " + clock + "</kbd><br>FW: <kbd>" + branch + " " + hash + "</kbd><br>FW date: <kbd>" + date + "</kbd></div></div>");
-
-           }
-
-              //$("#console").append(this + '<br/>');
+             var branch = branchgit[0];
+             var hash   = branchgit[1];
+             var date   = matches[2];
+             var mcu    = matches[3];
+             var clock  = matches[4];
+             $("#foundIp").append("<div class='panel panel-primary'><div class='panel-heading'><h3 class='panel-title'><a onclick='setIP(\""+ip+"\")' href='#'>"+ip+"</a></h3></div><div class='panel-body'>MCU: <kbd>"+mcu+ " @ " + clock + "</kbd><br>FW: <kbd>" + branch + " " + hash + "</kbd><br>FW date: <kbd>" + date + "</kbd></div></div>");
+         }
       });
     });
-  // $.ajax({
-  //  type: 'GET',
-  //     url: 'http://' +ip + '/command',
-  //     data: "version",
-  //     success: function(data, textStatus, XMLHttpRequest) {
-  //       console.log(data, textStatus, XMLHttpRequest);
-  //       if (textStatus = '200') {
-  //         // alert('Found board at' + ip)
-  //         printLog('Found '+ip, successcolor)
-  //         $("#foundIp").append(ip);
-  //       }
-  //     },
-  //     error: function(XMLHttpRequest, textStatus, errorThrown) {
-  //       console.log(XMLHttpRequest, textStatus, errorThrown)
-  //       printLog('Nothing on '+ip, errorcolor)
-  //     }
-  // });
+  posting.fail(function(data) {
+    scanned = scanned - 1;
+    scanfail += 1
+    $("#scannumber").html('Scanning: <span style="color: #00cc00">'+scanok+ '</span>+<span style="color: #cc0000">'+scanfail+ '</span> done. '+scanned+' to go.' )
+  });
 }
-
 
 function setIP(ipaddr) {
   $("#smoothieIp").val(ipaddr);
 }
 
-
 function runCommand(cmd, silent) {
   var host = $('#smoothieIp').val();
-
-    if (host) {
-        console.log('Connecting Smoothie at ', host);
-    } else {
-        host = '127.0.0.1'
-    }
-    // Get some values from elements on the page:
-    cmd += "\n";
-    url = silent ? "http://" + host + "/command_silent" : "http://" + host + "/command";
-    // Send the data using post
-    var posting = $.post(url, cmd);
-    // Put the results in a div
-    if (!silent) {
-        posting.done(function(data) {
-            $("#result").empty();
-            $.each(data.split('\n'), function(index) {
-                printLog(this, msgcolor)
-                    //$("#console").append(this + '<br/>');
-            });
-        });
-    }
+  if (host) {
+      // console.log('Connecting Smoothie at ', host);
+  } else {
+      host = '127.0.0.1'
+  }
+  cmd += "\n";
+  url = silent ? "http://" + host + "/command_silent" : "http://" + host + "/command";
+  // Send the data using post
+  var posting = $.post(url, cmd);
+  // Put the results in console
+  if (!silent) {
+    posting.done(function(data) {
+      $.each(data.split('\n'), function(index) {
+        printLog(this, msgcolor)
+        console.log(this);
+      });
+    });
+  }
 }
 
+function getStatus() {
+  var host = $('#smoothieIp').val();
+  if (host) {
+      // console.log('Connecting Smoothie at ', host);
+  } else {
+      host = '127.0.0.1'
+  }
+  // Get some values from elements on the page:
+  cmd = "get status\n";
+  url = "http://" + host + "/command";
+  // Send the data using post
+  var posting = $.post(url, cmd);
+  posting.done(function(data) {
+      $.each(data.split('\n'), function(index) {
+          // console.log(this);
+          updateStatus(this);
+      });
+  });
+}
+
+
 function getTemperature() {
-    var host = $('#smoothieIp').val();
-    //runCommand("M105", false);
-    var regex_temp = /(B|T(\d*)):\s*([+]?[0-9]*\.?[0-9]+)?/gi;
-
-    //    var test_data = "ok T:23.3 /0.0 @0 T1:23.4 /0.0 @0 B:24.8 /0.0 @0 P:29.4 /0.0 @0";
-    var posting = $.post("http://" + host + "/command", "M105\n");
-    posting.done(function(data) {
-        while ((result = regex_temp.exec(data)) !== null) {
-            var tool = result[1];
-            var value = result[3];
-
-            if (tool == "T") {
-                //$("#heat_actual_t0").html(value + "&deg;C");
-            } else if (tool == "T1") {
-                //$("#heat_actual_t1").html(value + "&deg;C");
-            }
-            if (tool == "B") {
-                //$("#heat_actual_bed").html(value + "&deg;C");
-
-            }
-        }
-    });
+  var host = $('#smoothieIp').val();
+  var regex_temp = /(B|T(\d*)):\s*([+]?[0-9]*\.?[0-9]+)?/gi;
+  //    var test_data = "ok T:23.3 /0.0 @0 T1:23.4 /0.0 @0 B:24.8 /0.0 @0 P:29.4 /0.0 @0";
+  var posting = $.post("http://" + host + "/command", "M105\n");
+  posting.done(function(data) {
+    while ((result = regex_temp.exec(data)) !== null) {
+      var tool = result[1];
+      var value = result[3];
+      if (tool == "T") {
+        //$("#heat_actual_t0").html(value + "&deg;C");
+      } else if (tool == "T1") {
+        //$("#heat_actual_t1").html(value + "&deg;C");
+      }
+      if (tool == "B") {
+        //$("#heat_actual_bed").html(value + "&deg;C");
+      }
+    }
+  });
 }
--- public/js/socketcomms.js
+++ public/js/socketcomms.js
@@ -1,207 +1,237 @@
 var socket, isConnected, playing, connectVia;
 
-  function initSocket() {
-    socket = io.connect(''); // socket.io init
-    socket.emit('firstLoad', 1);
+function initSocket() {
+  socket = io.connect(''); // socket.io init
+  socket.emit('firstLoad', 1);
 
-    socket.on('data', function (data) {
+  socket.on('data', function (data) {
+    $('#syncstatus').html('Socket OK');
+    isConnected = true;
+    if (data.indexOf('<') == 0) {
+      updateStatus(data);
+    } else if (data =='ok') {
+    printLog(data, '#cccccc')
+    } else {
+    printLog(data, msgcolor)
+    }
+  });
 
-      $('#syncstatus').html('Socket OK');
-      isConnected = true;
+  socket.on('ports', function (data) {
+    $('#syncstatus').html('Socket Init');
+    var options = $("#port");
+    for (i = 0; i< data.length; i++) {
+      options.append($("<option />").val(data[i].comName).text(data[i].comName));
+    }
+    $('#connect').removeClass('disabled')
+    // Might as well pre-select the last-used port and buffer
+    var lastUsed = localStorage.getItem("lastUsedPort");
+    var lastBaud = localStorage.getItem("lastUsedBaud");
+    $("#port option:contains(" + lastUsed + ")").attr('selected', 'selected');
+    $("#baud option:contains(" + lastBaud + ")").attr('selected', 'selected');
+	});
 
-      if (data.indexOf('<') == 0) {
-        // https://github.com/grbl/grbl/wiki/Configuring-Grbl-v0.8#---current-status
-        // remove first <
-        var t = data.substr(1);
+  socket.on('activePorts', function (data) {
+    console.log('activePorts' + data)
+  });
 
-          // remove last >
-        t = t.substr(0,t.length-2);
+  socket.on('connectStatus', function (data) {
+		console.log(data);
+    $('#connectStatus').html(data)
+    $('#syncstatus').html('Socket OK');
+	});
 
-        // split on , and :
-        t = t.split(/,|:/);
+  $('#refreshPort').on('click', function() {
+    $('#port').find('option').remove().end()
+    socket.emit('refreshPorts', 1);
+    $('#syncstatus').html('Socket Refreshed');
+  });
 
-        //<Idle,MPos:26.7550,0.0850,0.0000,WPos:26.7550,0.0850,0.0000>
-        //0 status
-        //1 MPos
-        //2 mx
-        //3 my
-        //4 mz
-        //5 WPos
-        //6 wx
-        //7 wy
-        //8 wz
-        if (t[0] == 'Alarm') {
-          $("#machineStatus").removeClass('badge-ok')
-          $("#machineStatus").addClass('badge-notify')
-          $("#machineStatus").removeClass('badge-warn')
-          $("#machineStatus").removeClass('badge-busy')
-        } else if (t[0] == 'Home') {
-          $("#machineStatus").removeClass('badge-ok')
-          $("#machineStatus").removeClass('badge-notify')
-          $("#machineStatus").removeClass('badge-warn')
-          $("#machineStatus").addClass('badge-busy')
-        } else if (t[0] == 'Hold') {
-          $("#machineStatus").removeClass('badge-ok')
-          $("#machineStatus").removeClass('badge-notify')
-          $("#machineStatus").addClass('badge-warn')
-          $("#machineStatus").removeClass('badge-busy')
-        } else if (t[0] == 'Idle') {
-          $("#machineStatus").addClass('badge-ok')
-          $("#machineStatus").removeClass('badge-notify')
-          $("#machineStatus").removeClass('badge-warn')
-          $("#machineStatus").removeClass('badge-busy')
-        } else if (t[0] == 'Run') {
-          $("#machineStatus").removeClass('badge-ok')
-          $("#machineStatus").removeClass('badge-notify')
-          $("#machineStatus").removeClass('badge-warn')
-          $("#machineStatus").addClass('badge-busy')
-        }
+  $('#connect').on('click', function() {
+    var portName = $('#port').val();
+    var baudRate = $('#baud').val();
+    socket.emit('connectTo', portName + ',' + baudRate);
+    isConnected = true;
+    localStorage.setItem("lastUsedPort", portName);
+    localStorage.setItem("lastUsedBaud", baudRate);
+    $('#closePort').removeClass('disabled')
+  });
 
+  $('#closePort').on('click', function() {
+    socket.emit('closePort', 1);
+    isConnected = false;
+    $('#closePort').addClass('disabled')
+    $('#machineStatus').html('Not Connected');
+    $("#machineStatus").removeClass('badge-ok')
+    $("#machineStatus").addClass('badge-notify')
+    $("#machineStatus").removeClass('badge-warn')
+    $("#machineStatus").removeClass('badge-busy')
+  });
 
-syncstatus
-        $('#machineStatus').html(t[0]);
-        $('#mX').html(t[6]);
-        $('#mY').html(t[7]);
-        $('#mZ').html(t[8]);
-          if (bullseye) {
-            setBullseyePosition(t[6], t[7], t[8]); // Also updates #mX #mY #mZ
-          }
-        } else if (data =='ok') {
-        printLog(data, '#cccccc')
-        } else {
-        printLog(data, msgcolor)
-      }
-    });
+  $('#sendCommand').on('click', function() {
+    var commandValue = $('#command').val();
+    sendGcode(commandValue);
+    $('#command').val('');
+  });
 
-    socket.on('ports', function (data) {
-      $('#syncstatus').html('Socket Init');
-      var options = $("#port");
-      for (i = 0; i< data.length; i++) {
-        options.append($("<option />").val(data[i].comName).text(data[i].comName));
-      }
-      $('#connect').removeClass('disabled')
-      // Might as well pre-select the last-used port and buffer
-      var lastUsed = localStorage.getItem("lastUsedPort");
-      var lastBaud = localStorage.getItem("lastUsedBaud");
-      $("#port option:contains(" + lastUsed + ")").attr('selected', 'selected');
-      $("#baud option:contains(" + lastBaud + ")").attr('selected', 'selected');
-
-  	});
-
-    socket.on('activePorts', function (data) {
-      console.log('activePorts' + data)
-    });
-
-    socket.on('connectStatus', function (data) {
-  		console.log(data);
-      $('#connectStatus').html(data)
-      $('#syncstatus').html('Socket OK');
-  	});
-
-    $('#refreshPort').on('click', function() {
-        $('#port').find('option').remove().end()
-        socket.emit('refreshPorts', 1);
-        $('#syncstatus').html('Socket Refreshed');
-
-
-    });
-
-    $('#connect').on('click', function() {
-      var portName = $('#port').val();
-      var baudRate = $('#baud').val();
-      socket.emit('connectTo', portName + ',' + baudRate);
-      isConnected = true;
-      localStorage.setItem("lastUsedPort", portName);
-      localStorage.setItem("lastUsedBaud", baudRate);
-      $('#closePort').removeClass('disabled')
-    });
-
-    $('#closePort').on('click', function() {
-      socket.emit('closePort', 1);
-      isConnected = false;
-      $('#closePort').addClass('disabled')
-      $('#machineStatus').html('Not Connected');
-      $("#machineStatus").removeClass('badge-ok')
-      $("#machineStatus").addClass('badge-notify')
-      $("#machineStatus").removeClass('badge-warn')
-      $("#machineStatus").removeClass('badge-busy')
-    });
-
-
-    $('#sendCommand').on('click', function() {
-        var commandValue = $('#command').val();
-        sendGcode(commandValue);
-        $('#command').val('');
-    });
-
-    socket.on('qCount', function (data) {
-      data = parseInt(data);
-      $('#queueCnt').html('Queued: ' + data)
-      if (data < 1) {
-          $('#playicon').removeClass('fa-pause');
-          $('#playicon').addClass('fa-play');
-          playing = false;
-          paused = false;
-      }
-    });
-
-
-  }
+  socket.on('qCount', function (data) {
+    data = parseInt(data);
+    $('#queueCnt').html('Queued: ' + data)
+    if (data < 1) {
+      $('#playicon').removeClass('fa-pause');
+      $('#playicon').addClass('fa-play');
+      playing = false;
+      paused = false;
+    }
+  });
+}
 
 function sendGcode(gcode) {
   console.log('Sending', gcode)
-  socket.emit('serialSend', gcode);
+  var connectVia = $('#connectVia').val()
+  if (connectVia == "USB") {
+    socket.emit('serialSend', gcode);
+  } else if (connectVia == "Ethernet") {
+    runCommand(gcode);
+  }
 }
 
 function stopMachine () {
   var laseroffcmd;
   laseroffcmd = document.getElementById('laseroff').value;
-  if (laseroffcmd) {
-    socket.emit('stop', laseroffcmd);
-  } else {
-    socket.emit('stop', 0);
+  var connectVia = $('#connectVia').val()
+  if (connectVia == "USB") {
+    if (laseroffcmd) {
+      socket.emit('stop', laseroffcmd);
+    } else {
+      socket.emit('stop', 0);
+    }
+  } else if (connectVia == "Ethernet") {
+    if (laseroffcmd) {
+      runCommand('abort');
+      runCommand(laseroffcmd);
+      runCommand('\030');
+      runCommand(laseroffcmd);
+    } else {
+      runCommand('abort');
+      runCommand('\030');
+    }
   }
 }
 
 function playpauseMachine() {
-    if (isConnected) {
-        if (playing) {
-            if (paused) {
-                // sendGcode('~');
-                socket.emit('unpause', 1);
-                paused = false;
-                $('#playicon').removeClass('fa-play');
-                $('#playicon').addClass('fa-pause');
-            } else {
-                var laseroffcmd;
-                laseroffcmd = document.getElementById('laseroff').value;
-                if (laseroffcmd) {
-                  socket.emit('pause', laseroffcmd);
-                } else {
-                  socket.emit('pause', 0);
-                }
-
-                paused = true;
-                $('#playicon').removeClass('fa-pause');
-                $('#playicon').addClass('fa-play');
-            }
-        } else {
-            playGcode();
+  if (isConnected) {
+    if (playing) {
+      if (paused) {
+        // sendGcode('~');
+        var connectVia = $('#connectVia').val()
+        if (connectVia == "USB") {
+          socket.emit('unpause', 1);
+        } else if (connectVia == "Ethernet") {
+          runCommand('resume');
         }
+        paused = false;
+        $('#playicon').removeClass('fa-play');
+        $('#playicon').addClass('fa-pause');
+      // end ifPaused
+      } else {
+        var laseroffcmd;
+        laseroffcmd = document.getElementById('laseroff').value;
+        if (laseroffcmd) {
+          var connectVia = $('#connectVia').val()
+          if (connectVia == "USB") {
+            socket.emit('pause', laseroffcmd);
+          } else if (connectVia == "Ethernet") {
+            runCommand('suspend');
+            runCommand(laseroffcmd)
+          }
+        } else {
+          if (connectVia == "USB") {
+            socket.emit('pause', 0);
+          } else if (connectVia == "Ethernet") {
+            runCommand('pause');
+          }
+        }
+        paused = true;
+        $('#playicon').removeClass('fa-pause');
+        $('#playicon').addClass('fa-play');
+      }
+    // end isPlaying
     } else {
-        printLog('You have to Connect to a machine First!', errorcolor)
+      playGcode();
     }
-
-
-
+  // end isConnected
+  } else {
+    printLog('You have to Connect to a machine First!', errorcolor)
+  }
 };
-
 
 function playGcode() {
-    var g;
-    g = document.getElementById('gcodepreview').value;
-    sendGcode(g);
-    playing = true;
-    $('#playicon').removeClass('fa-play');
-    $('#playicon').addClass('fa-pause');
+  var connectVia = $('#connectVia').val()
+  if (connectVia == "USB") {
+    if (isConnected) {
+      var g;
+      g = document.getElementById('gcodepreview').value;
+      sendGcode(g);
+      playing = true;
+      $('#playicon').removeClass('fa-play');
+      $('#playicon').addClass('fa-pause');
+    } else {
+      printLog('Not Connected', errorcolor)
+    }
+  } else if (connectVia == "Ethernet") {
+    // Upload to SD Wizard
+  }
 };
+
+function updateStatus(data) {
+  // https://github.com/grbl/grbl/wiki/Configuring-Grbl-v0.8#---current-status
+  // remove first <
+  var t = data.substr(1);
+    // remove last >
+  t = t.substr(0,t.length-2);
+  // split on , and :
+  t = t.split(/,|:/);
+  //<Idle,MPos:26.7550,0.0850,0.0000,WPos:26.7550,0.0850,0.0000>
+  //0 status
+  //1 MPos
+  //2 mx
+  //3 my
+  //4 mz
+  //5 WPos
+  //6 wx
+  //7 wy
+  //8 wz
+  if (t[0] == 'Alarm') {
+    $("#machineStatus").removeClass('badge-ok')
+    $("#machineStatus").addClass('badge-notify')
+    $("#machineStatus").removeClass('badge-warn')
+    $("#machineStatus").removeClass('badge-busy')
+  } else if (t[0] == 'Home') {
+    $("#machineStatus").removeClass('badge-ok')
+    $("#machineStatus").removeClass('badge-notify')
+    $("#machineStatus").removeClass('badge-warn')
+    $("#machineStatus").addClass('badge-busy')
+  } else if (t[0] == 'Hold') {
+    $("#machineStatus").removeClass('badge-ok')
+    $("#machineStatus").removeClass('badge-notify')
+    $("#machineStatus").addClass('badge-warn')
+    $("#machineStatus").removeClass('badge-busy')
+  } else if (t[0] == 'Idle') {
+    $("#machineStatus").addClass('badge-ok')
+    $("#machineStatus").removeClass('badge-notify')
+    $("#machineStatus").removeClass('badge-warn')
+    $("#machineStatus").removeClass('badge-busy')
+  } else if (t[0] == 'Run') {
+    $("#machineStatus").removeClass('badge-ok')
+    $("#machineStatus").removeClass('badge-notify')
+    $("#machineStatus").removeClass('badge-warn')
+    $("#machineStatus").addClass('badge-busy')
+  }
+  $('#machineStatus').html(t[0]);
+  $('#mX').html(t[6]);
+  $('#mY').html(t[7]);
+  $('#mZ').html(t[8]);
+  if (bullseye) {
+    setBullseyePosition(t[6], t[7], t[8]); // Also updates #mX #mY #mZ
+  }
+}
