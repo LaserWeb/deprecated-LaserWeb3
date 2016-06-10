7668d5a71d478aeafa8c8878ad6ba4ce4f2161cb
daned33 <danedwards33@gmail.com>
2016-06-11 00:51:32 +0300

    Fixed #11 (feedrate for rastergcode)

--- public/js/raster.js
+++ public/js/raster.js
@@ -107,10 +107,10 @@ function rasterInit() {
         var physwidth = (width / dpival) ;
         var spotSize = (physwidth / width);
         var spotSizeMul = parseFloat($('#spotSize').val());
-        var laserFeed = $('#feedRate').val();
-        var laserRapid = $('#rapidRate').val();
-        var blackspeed = $("#laservariablespeedslider").slider("values", 0) * laserRapid / 100.0;
-        var whitespeed = $("#laservariablespeedslider").slider("values", 1) * laserRapid / 100.0;
+        var laserFeed = $('#feedRate').val() * 60;
+        var laserRapid = $('#rapidRate').val() * 60;
+        var blackspeed = $("#laservariablespeedslider").slider("values", 0) * laserRapid / 100.0 * 60;
+        var whitespeed = $("#laservariablespeedslider").slider("values", 1) * laserRapid / 100.0 * 60;
         var useVariableSpeed = $('#useRasterBlackWhiteSpeeds').prop('checked');
         $('#rasterProgressShroud').hide();
         var xoffset = parseFloat($('#rasterxoffset').val());
