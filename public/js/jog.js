578c0e44db109e77362be71389f37ab2b8099331
openhardwarecoza <peter.plaaswerf@gmail.com>
2016-06-12 15:38:28 +0200

    Set feedrate for jog moves - backend done fixed #15

--- public/js/jog.js
+++ public/js/jog.js
@@ -11,48 +11,54 @@ function initJog() {
     $('#xP').on('click', function() {
        if (isConnected) {
          var dist = $('input[name=stp]:checked', '#stepsize').val()
+         var feedrate = $('#jogfeedxy').val()
          console.log('Jog Distance', dist);
-         sendGcode('G91\nG0 X'+ dist + '\nG90\n');
+         sendGcode('G91\nG0 F'+ feedrate +' X'+ dist + '\nG90\n');
        }
     });
 
     $('#yP').on('click', function() {
        if (isConnected) {
          var dist = $('input[name=stp]:checked', '#stepsize').val()
+         var feedrate = $('#jogfeedxy').val()
          console.log('Jog Distance', dist);
-         sendGcode('G91\nG0 Y'+ dist + '\nG90\n');
+         sendGcode('G91\nG0 F'+ feedrate +' Y'+ dist + '\nG90\n');
        }
     });
 
     $('#zP').on('click', function() {
        if (isConnected) {
          var dist = $('input[name=stp]:checked', '#stepsize').val()
+         var feedrate = $('#jogfeedz').val()
          console.log('Jog Distance', dist);
-         sendGcode('G91\nG0 Z'+ dist + '\nG90\n');
+         sendGcode('G91\nG0 F'+ feedrate +' Z'+ dist + '\nG90\n');
        }
     });
 
     $('#xM').on('click', function() {
        if (isConnected) {
          var dist = $('input[name=stp]:checked', '#stepsize').val()
+         var feedrate = $('#jogfeedxy').val()
          console.log('Jog Distance', dist);
-         sendGcode('G91\nG0 X-'+ dist + '\nG90\n');
+         sendGcode('G91\nG0 F'+ feedrate +' X-'+ dist + '\nG90\n');
        }
     });
 
     $('#yM').on('click', function() {
        if (isConnected) {
          var dist = $('input[name=stp]:checked', '#stepsize').val()
+         var feedrate = $('#jogfeedxy').val()
          console.log('Jog Distance', dist);
-         sendGcode('G91\nG0 Y-'+ dist + '\nG90\n');
+         sendGcode('G91\nG0 F'+ feedrate +' Y-'+ dist + '\nG90\n');
        }
     });
 
     $('#zM').on('click', function() {
        if (isConnected) {
          var dist = $('input[name=stp]:checked', '#stepsize').val()
+         var feedrate = $('#jogfeedz').val()
          console.log('Jog Distance', dist);
-         sendGcode('G91\nG0 Z-'+ dist + '\nG90\n');
+         sendGcode('G91\nG0 F'+ feedrate +' Z-'+ dist + '\nG90\n');
        }
     });
 
