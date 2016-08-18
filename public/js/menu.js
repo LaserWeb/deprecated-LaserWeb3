5d810266adbeda18119e1de75cc418698d3b78fa
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2016-08-18 08:20:34 +0200

    Put menu and google-analytics scripts in dedicated file

---
+++ public/js/menu.js
@@ -0,0 +1,41 @@
+$("#cam-menu").click(function() {
+    $('.mobtab').hide();
+    $('.leftmenuitem').removeClass('active');
+    $('#cam-menu-panel').show();
+    $("#cam-menu").addClass('active')
+});
+
+$("#settings-menu").click(function() {
+    $('.mobtab').hide();
+    $('.leftmenuitem').removeClass('active');
+    $('#settings-menu-panel').show();
+    $("#settings-menu").addClass('active')
+});
+
+$("#gcode-menu").click(function() {
+    $('.mobtab').hide();
+    $('.leftmenuitem').removeClass('active');
+    $('#gcode-menu-panel').show();
+    $("#gcode-menu").addClass('active')
+});
+
+$("#quote-menu").click(function() {
+    $('.mobtab').hide();
+    $('.leftmenuitem').removeClass('active');
+    $('#quote-menu-panel').show();
+    $("#quote-menu").addClass('active')
+});
+
+$("#tree-cam-menu").click(function() {
+    $('.mobtab').hide();
+    $('.leftmenuitem').removeClass('active');
+    $('#tree-cam-menu-panel').show();
+    $("#tree-cam-menu").addClass('active')
+});
+
+$("#jog-menu").click(function() {
+    $('.mobtab').hide();
+    $('.leftmenuitem').removeClass('active');
+    $('#jog-menu-panel').show();
+    $("#jog-menu").addClass('active')
+});
