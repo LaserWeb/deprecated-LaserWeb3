1590a1001ee1cb3e316ff20af6808a15cba0d418
Monte Krol <krolco@hotmail.com>
2016-09-06 00:21:44 -0700

    Refactored menu.js to reduce repeated sections

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,41 +1,26 @@
-$("#cam-menu").click(function() {
-    $('.mobtab').hide();
-    $('.leftmenuitem').removeClass('active');
-    $('#cam-menu-panel').show();
-    $("#cam-menu").addClass('active')
-});
+var appMenus = [
+    "cam",
+    "settings",
+    "gcode",
+    "quote",
+    "tree-cam",
+    "jog",
+    "stats"
+];
 
-$("#settings-menu").click(function() {
+function makeTabActive(tabname) {
+    var menuName = "#" + tabname + "-menu";
+    var panelName = menuName + "-panel";
     $('.mobtab').hide();
     $('.leftmenuitem').removeClass('active');
-    $('#settings-menu-panel').show();
-    $("#settings-menu").addClass('active')
-});
+    $(panelName).show();
+    $(menuName).addClass('active');
+}
 
-$("#gcode-menu").click(function() {
-    $('.mobtab').hide();
-    $('.leftmenuitem').removeClass('active');
-    $('#gcode-menu-panel').show();
-    $("#gcode-menu").addClass('active')
-});
-
-$("#quote-menu").click(function() {
-    $('.mobtab').hide();
-    $('.leftmenuitem').removeClass('active');
-    $('#quote-menu-panel').show();
-    $("#quote-menu").addClass('active')
-});
-
-$("#tree-cam-menu").click(function() {
-    $('.mobtab').hide();
-    $('.leftmenuitem').removeClass('active');
-    $('#tree-cam-menu-panel').show();
-    $("#tree-cam-menu").addClass('active')
-});
-
-$("#jog-menu").click(function() {
-    $('.mobtab').hide();
-    $('.leftmenuitem').removeClass('active');
-    $('#jog-menu-panel').show();
-    $("#jog-menu").addClass('active')
-});
+// Each menu name gets an id handler of this pattern: #[menuname]-menu 
+appMenus.forEach(function(menu) {
+    console.log("Adding menu handler for " + menu);
+    $("#" + menu + "-menu").click(function() {
+        makeTabActive(menu);
+    });
+}, this);
