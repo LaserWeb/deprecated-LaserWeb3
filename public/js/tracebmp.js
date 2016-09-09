6f20193f4a4ea4ff11c5333c4775db3aab10600a
Monte Krol <krolco@hotmail.com>
2016-09-09 12:06:20 -0700

    Added modal panel while tracing is happening

--- public/js/tracebmp.js
+++ public/js/tracebmp.js
@@ -1,6 +1,12 @@
 function tracebmp(index, filename) {
-  var seq = objectsInScene[index].userData.seq;
-  var toTrace = 'origImage'+seq;
-  Potrace.loadImageFromUrl(objectsInScene[index].userData.imgdata);
-  Potrace.process(function(){svg2three(Potrace.getSVG(1), "trace"+filename+".svg" ); fillLayerTabs(); fillTree(); });
+	$("#tracingmodal").modal('show');
+	var seq = objectsInScene[index].userData.seq;
+	var toTrace = 'origImage'+seq;
+	Potrace.loadImageFromUrl(objectsInScene[index].userData.imgdata);
+	Potrace.process(function(){
+		svg2three(Potrace.getSVG(1), "trace"+filename+".svg" );
+		fillLayerTabs();
+		fillTree();
+		$("#tracingmodal").modal('hide');
+	});
 }
