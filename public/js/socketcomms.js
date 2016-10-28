84ba6bafb51421f1e13b020d20964558d98e66d9
cprezzi <cprezzi@comsulting.ch>
2016-10-28 16:55:08 +0200

    Checked status report for Smoothie syntax.

--- public/js/socketcomms.js
+++ public/js/socketcomms.js
@@ -256,6 +256,7 @@ function homeMachine() {
 };
 
 function updateStatus(data) {
+  // Smoothieware: <Idle,MPos:49.5756,279.7644,-15.0000,WPos:0.0000,0.0000,0.0000>
   // till GRBL v0.9: <Idle,MPos:0.000,0.000,0.000,WPos:0.000,0.000,0.000>
   // since GRBL v1.1: <Idle|WPos:0.000,0.000,0.000|Bf:15,128|FS:0,0|Pn:S|WCO:0.000,0.000,0.000> (when $10=2!)
 
