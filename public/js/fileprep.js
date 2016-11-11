b8511af4be37d88d97cac495d220c6bad67de12a
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2016-11-11 16:26:25 +0100

    Indentation

--- public/js/fileprep.js
+++ public/js/fileprep.js
@@ -6,92 +6,92 @@ var oldyscale = 0;
 
 function filePrepInit() {
 
-  $('#panleft').on('click', function() {
-    var oldValue = lw.viewer.viewControls.target.x
-    var newVal = oldValue + 20;
-    TweenMax.to(lw.viewer.viewControls.target,0.25,{x:newVal,onUpdate:function(){
-                        lw.viewer.viewControls.update();
-                         }});
-  });
+    $('#panleft').on('click', function() {
+        var oldValue = lw.viewer.viewControls.target.x
+        var newVal = oldValue + 20;
+        TweenMax.to(lw.viewer.viewControls.target,0.25,{x:newVal,onUpdate:function(){
+            lw.viewer.viewControls.update();
+        }});
+    });
 
-  $('#panright').on('click', function() {
-    var oldValue = lw.viewer.viewControls.target.x
-    var newVal = oldValue - 20;
-    TweenMax.to(lw.viewer.viewControls.target,0.25,{x:newVal,onUpdate:function(){
-                        lw.viewer.viewControls.update();
-                         }});
-  });
+    $('#panright').on('click', function() {
+        var oldValue = lw.viewer.viewControls.target.x
+        var newVal = oldValue - 20;
+        TweenMax.to(lw.viewer.viewControls.target,0.25,{x:newVal,onUpdate:function(){
+            lw.viewer.viewControls.update();
+        }});
+    });
 
-  $('#panup').on('click', function() {
-    var oldValue = lw.viewer.viewControls.target.y
-    var newVal = oldValue - 20;
-    TweenMax.to(lw.viewer.viewControls.target,0.25,{y:newVal,onUpdate:function(){
-                        lw.viewer.viewControls.update();
-                         }});
-  });
+    $('#panup').on('click', function() {
+        var oldValue = lw.viewer.viewControls.target.y
+        var newVal = oldValue - 20;
+        TweenMax.to(lw.viewer.viewControls.target,0.25,{y:newVal,onUpdate:function(){
+            lw.viewer.viewControls.update();
+        }});
+    });
 
-  $('#pandown').on('click', function() {
-    var oldValue = lw.viewer.viewControls.target.y
-    var newVal = oldValue + 20;
-    TweenMax.to(lw.viewer.viewControls.target,0.25,{y:newVal,onUpdate:function(){
-                        lw.viewer.viewControls.update();
-                         }});
-  });
+    $('#pandown').on('click', function() {
+        var oldValue = lw.viewer.viewControls.target.y
+        var newVal = oldValue + 20;
+        TweenMax.to(lw.viewer.viewControls.target,0.25,{y:newVal,onUpdate:function(){
+            lw.viewer.viewControls.update();
+        }});
+    });
 
-  $('#zoomout').on('click', function() {
-    TweenMax.to(lw.viewer.camera,0.25,{fov:"+=5",onUpdate:function(){
-                        lw.viewer.camera.updateProjectionMatrix();
-                         }});
-    lw.viewer.viewControls.update();
-  });
+    $('#zoomout').on('click', function() {
+        TweenMax.to(lw.viewer.camera,0.25,{fov:"+=5",onUpdate:function(){
+            lw.viewer.camera.updateProjectionMatrix();
+        }});
+        lw.viewer.viewControls.update();
+    });
 
-  $('#zoomin').on('click', function() {
-    TweenMax.to(lw.viewer.camera,0.25,{fov:"-=5",onUpdate:function(){
-                        lw.viewer.camera.updateProjectionMatrix();
-                         }});
-    lw.viewer.viewControls.update();
-  });
+    $('#zoomin').on('click', function() {
+        TweenMax.to(lw.viewer.camera,0.25,{fov:"-=5",onUpdate:function(){
+            lw.viewer.camera.updateProjectionMatrix();
+        }});
+        lw.viewer.viewControls.update();
+    });
 
 }
 
 function resetView() {
-  if (objectsInScene.length > 0) {
-      var insceneGrp = new THREE.Group()
-      for (i = 0; i < objectsInScene.length; i++) {
-        var object = objectsInScene[i].clone();
-        insceneGrp.add(object)
-      }
-      viewExtents(insceneGrp);
+    if (objectsInScene.length > 0) {
+        var insceneGrp = new THREE.Group()
+        for (i = 0; i < objectsInScene.length; i++) {
+            var object = objectsInScene[i].clone();
+            insceneGrp.add(object)
+        }
+        viewExtents(insceneGrp);
     } else {
-      viewExtents(helper);
-  }
+        viewExtents(helper);
+    }
 
 }
 
 function putFileObjectAtZero(object) {
 
-  if (object) {
-  } else {
-      object = fileParentGroup
-  }
+    if (object) {
+    } else {
+        object = fileParentGroup
+    }
 
-  var bbox2 = new THREE.Box3().setFromObject(object);
-  Xtofix = -(bbox2.min.x + (laserxmax / 2));
-  imagePosition = $('#imagePosition').val()
-  // console.log('ImagePosition', imagePosition)
-  if (imagePosition == "TopLeft") {
-      Ytofix = (laserymax / 2) - bbox2.max.y;
-  } else {
-      Ytofix = -(bbox2.min.y + (laserymax / 2));
-  }
-  object.translateX(Xtofix);
-  object.translateY(Ytofix);
-  calcZeroOffset(object);
+    var bbox2 = new THREE.Box3().setFromObject(object);
+    Xtofix = -(bbox2.min.x + (laserxmax / 2));
+    imagePosition = $('#imagePosition').val()
+    // console.log('ImagePosition', imagePosition)
+    if (imagePosition == "TopLeft") {
+        Ytofix = (laserymax / 2) - bbox2.max.y;
+    } else {
+        Ytofix = -(bbox2.min.y + (laserymax / 2));
+    }
+    object.translateX(Xtofix);
+    object.translateY(Ytofix);
+    calcZeroOffset(object);
 
 }
 
 function calcZeroOffset(object) {
-      if (object) {
+    if (object) {
         var bbox2 = new THREE.Box3().setFromObject(object);
         xfromzero = -(bbox2.min.x + (laserxmax / 2));
         imagePosition = $('#imagePosition').val()
