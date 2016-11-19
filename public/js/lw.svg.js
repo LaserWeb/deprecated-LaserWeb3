c16ab8366a7c4eebe8bcf367c0eac4db661bce5c
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2016-11-19 09:36:04 +0100

    (Re)add SVG drawing methods

--- public/js/lw.svg.js
+++ public/js/lw.svg.js
@@ -15,6 +15,113 @@ var lw = lw || {};
 
     // -------------------------------------------------------------------------
 
+    lw.svg.createColor = function(color) {
+        // TODO ...
+        //return new THREE.Color('red');
+        color = new THREE.Color(color);
+        var r = color.r * 255;
+        var g = color.g * 255;
+        var b = color.b * 255;
+
+        // Darken too light colors...
+        var luma, lumaLimit = 200;
+
+        while (true) {
+            luma = (r * 0.3) + (g * 0.59) + (b * 0.11);
+
+            if (luma <= lumaLimit) {
+                break;
+            }
+
+            r > 0 && (r -= 1);
+            g > 0 && (g -= 1);
+            b > 0 && (b -= 1);
+        }
+
+        // Create color object ([0-255] to [0-1] range)
+        color = new THREE.Color(r / 255, g / 255, b / 255);
+
+
+        // Return the color object
+        return color;
+    };
+
+    // -------------------------------------------------------------------------
+
+    lw.svg.createLineMaterial = function(tag) {
+        return new THREE.LineBasicMaterial({ color: this.createColor(
+            tag.getAttr('stroke', tag.getAttr('fill', 'black'))
+        ) });
+    };
+
+    // -------------------------------------------------------------------------
+
+    lw.svg.createSolidMaterial = function(tag) {
+        var opacity  = tag.getAttr('fillOpacity', 1);
+        var material = new THREE.MeshBasicMaterial({
+            color: tag.getAttr('fill', 'black')
+        });
+
+        if (opacity < 1) {
+            material.transparent = true;
+            material.opacity     = opacity;
+        }
+
+        return material;
+    };
+
+    // -------------------------------------------------------------------------
+
+    lw.svg.drawLine = function(tag, path) {
+        var geometry = new THREE.Geometry();
+        var material = this.createLineMaterial(tag);
+
+        path.points.forEach(function(point) {
+            geometry.vertices.push(new THREE.Vector3(point.x, point.y, 0));
+        });
+
+        return new THREE.Line(geometry, material);
+    };
+
+    // -------------------------------------------------------------------------
+
+    lw.svg.drawShape = function(tag, path) {
+        var shape    = new THREE.Shape(path.points);
+        var geometry = new THREE.ShapeGeometry(shape);
+        var material = this.createSolidMaterial(tag);
+
+        return new THREE.Mesh(geometry, material);
+    };
+
+    // -------------------------------------------------------------------------
+
+    lw.svg.drawTag = function(tag) {
+        // Create 3D object
+        var object = new THREE.Object3D();
+
+        // Draw object paths
+        if (tag.paths.length) {
+            this.info('draw:', tag);
+
+            tag.paths.map(function(path) {
+                if (tag.getAttr('fill', 'none') !== 'none') {
+                    object.add(this.drawShape(tag, path));
+                }
+
+                object.add(this.drawLine(tag, path));
+            }, this);
+        }
+
+        // Draw children
+        tag.children.forEach(function(childTag) {
+            object.add(this.drawTag(childTag));
+        }, this);
+
+        return object;
+    };
+
+    // -------------------------------------------------------------------------
+
     lw.svg.drawFile = function(file, name, settings) {
         // Default settings
         settings          = settings          || {};
@@ -31,10 +138,8 @@ var lw = lw || {};
             var svg    = parser.parse(file);
             this.logEnd();
 
-            //this.debug('parser:', parser);
-
             this.logStart('Drawing SVG: ' + name);
-            var object = new THREE.Object3D();
+            var object = lw.svg.drawTag(svg);
         }
         catch (error) {
             // Call user callback
